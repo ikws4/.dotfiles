65f72c7f00bd9ea426bd5c69fad7bc14459c7b5f
ikws4 <zhipingne@gmail.com>
2021-06-20 20:49:02 +0800

    feat: add padding for bufferline

--- .config/nvim/lua/myconfig/bufferline/init.lua
+++ .config/nvim/lua/myconfig/bufferline/init.lua
@@ -1,22 +1,37 @@
 require('bufferline').setup{
     options = {
-        mappings = true,
+        mappings = false,
         left_mouse_command = function(bufnum)
-           require('bufdelete').bufdelete(bufnum, false)
+            require('bufdelete').bufdelete(bufnum, false)
         end,
-        offsets = {{filetype = 'NvimTree', text = ' File Explore', text_align = 'left'}},
+        offsets = {
+            {
+                filetype = 'NvimTree',
+                text = ' File Explore',
+                text_align = 'left',
+                padding = 1
+            }
+        },
+        indicator_icon = '|',
         show_buffer_icons = true,
         show_buffer_close_icons = true,
         show_close_icon = true,
         show_tab_indicators = true,
         persist_buffer_sort = true,
-        separator_style = 'slant',
+        separator_style = "slant" or "padded_slant",
         enforce_regular_tabs = false,
         always_show_bufferline = true,
-        sort_by = 'extension',
+        sort_by = function(a, b)
+            local astat = vim.loop.fs_stat(a.path)
+            local bstat = vim.loop.fs_stat(b.path)
+            local mod_a = astat and astat.mtime.sec or 0
+            local mod_b = bstat and bstat.mtime.sec or 0
+            return mod_a > mod_b
+        end,
         diagnostics = 'nvim_lsp',
         diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            return '('..count..')'
+            local icon = level:match('error') and ' ' or ' '
+            return ' ' .. icon .. count
         end,
     }
 }
