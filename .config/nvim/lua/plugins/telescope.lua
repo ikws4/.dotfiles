60b93073d7cb75993321092ca38c69946f8afd4a
ikws4 <zhipingne@gmail.com>
2021-07-20 17:35:43 +0800

    nvim(telescope): file_finder theme config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,24 @@
 return function()
+  local actions = require "telescope.actions"
+
+  require "telescope".setup {
+    defaults = {
+      mappings = {
+        i = {
+          ["<esc>"] = actions.close
+        }
+      }
+    }
+  }
+
   local map = vim.api.nvim_set_keymap
   local opts = {noremap = true, silent = true}
-
-  map("n", "<leader><space>", [[<cmd>lua require('telescope.builtin').buffers()<CR>]], opts)
-  map("n", "<leader>sf", [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], opts)
+  map("n", "<leader><space>", [[<cmd>Telescope buffers<CR>]], opts)
+  map(
+    "n",
+    "<leader>sf",
+    [[<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown({previewer = false}))<CR>]],
+    opts
+  )
   map("n", "<leader>sb", [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], opts)
 end
