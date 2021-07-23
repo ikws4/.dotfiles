625cd9069df73ce852c623be9719963821d0c214
ikws4 <zhipingne@gmail.com>
2021-07-23 09:59:10 +0800

    nvim(FTerm): change keymapping

--- .config/nvim/lua/plugins/FTerm.lua
+++ .config/nvim/lua/plugins/FTerm.lua
@@ -1,5 +1,5 @@
 return function()
   require("FTerm").setup()
-  vim.api.nvim_set_keymap("n", "<leader>m", [[<cmd>lua require("FTerm").open()<CR>]], {noremap = true, silent = true})
+  vim.api.nvim_set_keymap("n", "<leader>t", [[<cmd>lua require("FTerm").open()<CR>]], {noremap = true, silent = true})
   vim.api.nvim_set_keymap("t", "<esc>", [[<cmd>lua require("FTerm").close()<CR>]], {noremap = true, silent = true})
 end
