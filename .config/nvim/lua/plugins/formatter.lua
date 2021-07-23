3cd4ffb7a048ae104cf38e8e42d531eb4737d690
ikws4 <zhipingne@gmail.com>
2021-07-23 09:57:05 +0800

    refactor: use stylua

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,29 +1,35 @@
 return function()
-  require("formatter").setup(
-    {
-      logging = false,
-      filetype = {
-        typescript = {
-          function()
-            return {
-              exe = "prettier",
-              args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
-              stdin = true
-            }
-          end
-        },
-        lua = {
-          function()
-            return {
-              exe = "luafmt",
-              args = {"--indent-count", 2, "--stdin"},
-              stdin = true
-            }
-          end
-        }
-      }
-    }
-  )
+  require("formatter").setup({
+    logging = false,
+    filetype = {
+      typescript = {
+        function()
+          return {
+            exe = "prettier",
+            args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
+            stdin = true,
+          }
+        end,
+      },
+      lua = {
+        function()
+          return {
+            exe = "stylua",
+            args = {
+              "--indent-width",
+              2,
+              "--indent-type",
+              "Spaces",
+              "--stdin-filepath",
+              vim.api.nvim_buf_get_name(0),
+              "-",
+            },
+            stdin = true,
+          }
+        end,
+      },
+    },
+  })
 
   vim.api.nvim_exec(
     [[
