be8e79ba699279365728a559bb1a0a404f6fcfee
ikws4 <zhipingne@gmail.com>
2021-07-23 11:11:53 +0800

    nvim(hop): update keymapping

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -1,9 +1,8 @@
 return function()
-	local map = vim.api.nvim_set_keymap
-	local opts = { noremap = true, silent = true }
+  local map = vim.api.nvim_set_keymap
+  local opts = { noremap = true, silent = true }
 
-	require("hop").setup()
+  require("hop").setup()
 
-	map("n", "fw", [[<cmd>HopWord<CR>]], opts)
-	map("n", "fl", [[<cmd>HopLine<CR>]], opts)
+  map("n", "ff", [[<cmd>HopChar2<CR>]], opts)
 end
