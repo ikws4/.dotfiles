6ca25b9c0ddcf0c8ef3afd24ef288ba52b7e56f7
ikws4 <zhipingne@gmail.com>
2021-08-15 19:05:55 +0800

    nvim(theme): enable diagnostic text hl

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -2,6 +2,7 @@ vim.o.fillchars = "eob: ,"
 vim.g.sonokai_style = "atlantis"
 vim.g.sonokai_show_eob = 0
 vim.g.sonokai_transparent_background = 1
+vim.g.sonokai_diagnostic_text_highlight = 1
 
 function _G.sonokai_custom()
   vim.cmd "hi! VertSplit guifg=#282B3C"
