828718d1a889bc037122ce7788fe933cb726365b
ikws4 <zhipingne@gmail.com>
2021-08-16 17:58:52 +0800

    nvim(lsp): use nvim-lsputils

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -224,23 +224,28 @@ local function plugins(use)
     end,
   }
   use {
-    "glepnir/lspsaga.nvim",
+    "RishabhRD/nvim-lsputils",
     config = function()
-      require("lspsaga").init_lsp_saga {
-        use_saga_diagnostic_sign = false,
-        code_action_prompt = {
-          enable = false,
-        },
-        rename_prompt_prefix = ">",
-        code_action_keys = {
-          quit = "<esc>",
-          exec = "<CR>",
-        },
-        rename_action_keys = {
-          quit = "<esc>",
-          exec = "<CR>",
-        },
-      }
+      vim.lsp.handlers["textDocument/codeAction"] = require("lsputil.codeAction").code_action_handler
+      vim.lsp.handlers["textDocument/references"] = require("lsputil.locations").references_handler
+      vim.lsp.handlers["textDocument/definition"] = require("lsputil.locations").definition_handler
+      vim.lsp.handlers["textDocument/declaration"] = require("lsputil.locations").declaration_handler
+      vim.lsp.handlers["textDocument/typeDefinition"] = require("lsputil.locations").typeDefinition_handbabaier
+      vim.lsp.handlers["textDocument/implementation"] = require("lsputil.locations").implementation_handler
+      vim.lsp.handlers["textDocument/documentSymbol"] = require("lsputil.symbols").document_handler
+      vim.lsp.handlers["workspace/symbol"] = require("lsputil.symbols").workspace_handler
+    end,
+    requires = { "RishabhRD/popfix" },
+  }
+  use {
+    "kosayoda/nvim-lightbulb",
+    config = function()
+      vim.o.updatetime = 250
+      vim.fn.sign_define(
+        "LightBulbSign",
+        { texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning" }
+      )
+      vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
     end,
   }
   use {
