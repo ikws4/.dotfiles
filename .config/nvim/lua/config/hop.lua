73e44c8d0bb12a64b9584da810cd1164399928aa
ikws4 <zhipingne@gmail.com>
2021-08-18 11:09:30 +0800

    nvim(hop): make to f to forward and F to backward

--- .config/nvim/lua/config/hop.lua
+++ .config/nvim/lua/config/hop.lua
@@ -1,3 +1,4 @@
 require("hop").setup()
 
-vim.api.nvim_set_keymap("n", "f", [[<cmd>HopChar2<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "F", [[<cmd>HopChar2BC<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "f", [[<cmd>HopChar2AC<CR>]], { noremap = true, silent = true })
