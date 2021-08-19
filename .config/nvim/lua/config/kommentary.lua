b111b5407cda18b39e01b239d6600bf6ce2625ca
ikws4 <zhipingne@gmail.com>
2021-08-19 22:17:28 +0800

    nvim(kommentary): add support for dart

--- .config/nvim/lua/config/kommentary.lua
+++ .config/nvim/lua/config/kommentary.lua
@@ -3,3 +3,8 @@ local kommentary = require "kommentary.config"
 kommentary.configure_language("default", {
   prefer_single_line_comments = true,
 })
+
+kommentary.configure_language("dart", {
+  single_line_comment_string = "//",
+  prefer_single_line_comments = true,
+})
