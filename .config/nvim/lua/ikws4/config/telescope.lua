31738f0b0775bc0359efd11689e5e57514fb9e78
ikws4 <zhipingne@gmail.com>
2021-08-29 12:30:55 +0800

    nvim: cleanup keymaps

--- .config/nvim/lua/ikws4/config/telescope.lua
+++ .config/nvim/lua/ikws4/config/telescope.lua
@@ -1,6 +1,7 @@
 local telescope = require "telescope"
 local actions = require "telescope.actions"
 local themes = require "telescope.themes"
+local wk = require "which-key"
 
 local lsp_preview_theme = themes.get_ivy {
   layout_config = {
@@ -48,7 +49,7 @@ telescope.setup {
       sorting_strategy = "ascending",
       hidden = true,
       layout_config = {
-        prompt_position = 'top',
+        prompt_position = "top",
       },
     },
   },
@@ -63,3 +64,13 @@ telescope.setup {
 }
 
 telescope.load_extension "fzf"
+
+wk.register {
+  ["<leader>ff"] = { "<Cmd>Telescope find_files hidden=true<CR>", "Find File" },
+  ["<leader>fr"] = { "<Cmd>Telescope oldfiles<CR>", "Open Recent File" },
+  ["<leader>bb"] = { "<Cmd>Telescope buffers<CR>", "Switch buffer" },
+  ["<leader>sh"] = { "<Cmd>Telescope help_tags<CR>", "Search help" },
+  ["<leader>sb"] = { "<Cmd>Telescope current_buffer_fuzzy_find<CR>", "Search buffer" },
+  ["<leader>sB"] = { "<cmd>Telescope live_grep grep_open_files=true<CR>", "Search all open buffers" },
+  ["<leader>sp"] = { "<Cmd>Telescope live_grep<CR>", "Search project" },
+}
