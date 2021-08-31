e67119d2dc52e102cf0bbaad24c49297ec266b82
ikws4 <zhipingne@gmail.com>
2021-08-31 10:46:56 +0800

    nvim(auto-pair): disable auto select

--- .config/nvim/lua/ikws4/plugins.lua
+++ .config/nvim/lua/ikws4/plugins.lua
@@ -119,7 +119,7 @@ local function plugins(use)
       require("nvim-autopairs.completion.cmp").setup {
         map_cr = true, --  map <CR> on insert mode
         map_complete = true, -- it will auto insert `(` after select function or method item
-        auto_select = true, -- automatically select the first item
+        auto_select = false, -- automatically select the first item
       }
     end,
   }
