a433ed7bc54404e38da6a64e17fd4b2b1bb3c5c5
ikws4 <zhipingne@gmail.com>
2021-09-06 22:34:21 +0800

    nvim: disable neogit buildtin notification
    Use notify.nvim api

--- .config/nvim/lua/ikws4/plugins.lua
+++ .config/nvim/lua/ikws4/plugins.lua
@@ -404,6 +404,7 @@ local function plugins(use)
     config = function()
       require("neogit").setup {
         disable_commit_confirmation = true,
+        disable_builtin_notifications = true,
         signs = {
           section = { "", "" },
           item = { "", "" },
