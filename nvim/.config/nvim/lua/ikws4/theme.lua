58d62c7c5a313f95e6290a250fdefef1dbe54a9e
ikws4 <zhipingne@gmail.com>
2021-10-01 17:53:46 +0800

    nvim(theme): override

--- nvim/.config/nvim/lua/ikws4/theme.lua
+++ nvim/.config/nvim/lua/ikws4/theme.lua
@@ -3,6 +3,12 @@ vim.g.rose_pine_variant = "moon"
 vim.g.rose_pine_bold_vertical_split_line = true
 
 function _G.themeOverride()
+  -- TODO: Make PR to rose-pine
+  vim.cmd("hi FloatBorder guibg=#232136 guifg=#59546d")
+  vim.cmd("hi TelescopeBorder guifg=#59546d")
+  vim.cmd("hi StatusLine guifg=#e0def4 guibg=#232136")
+  vim.cmd("hi StatusLineNC guifg=#817c9c guibg=#232136")
+  vim.cmd("hi! link NvimTreeNormal DarkenedPanel")
 end
 
 -- Highlight on yank
