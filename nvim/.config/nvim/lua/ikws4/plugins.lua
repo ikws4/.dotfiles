ccd85e45207fbb6919b5ced70dd032ca25af45c8
ikws4 <zhipingne@gmail.com>
2021-10-02 18:03:37 +0800

    nvim(todo-comment): remove icon

--- nvim/.config/nvim/lua/ikws4/plugins.lua
+++ nvim/.config/nvim/lua/ikws4/plugins.lua
@@ -200,6 +200,18 @@ local function plugins(use)
     config = function()
       require("todo-comments").setup {
         signs = false,
+        keywords = {
+          FIX = {
+            icon = "",
+            color = "error",
+            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+          },
+          TODO = { icon = "", color = "info" },
+          HACK = { icon = "", color = "warning" },
+          WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = "", color = "hint", alt = { "INFO" } },
+        },
         highlight = {
           keyword = "fg",
         },
