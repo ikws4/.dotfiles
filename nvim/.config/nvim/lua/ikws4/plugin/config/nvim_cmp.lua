265f2d98f8d449d75c877e100e66381ca089cc77
ikws4 <zhipingne@gmail.com>
2021-11-13 14:58:23 +0800

    nvim(cmp): disable <cr> for auto select first item

--- nvim/.config/nvim/lua/ikws4/plugin/config/nvim_cmp.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/config/nvim_cmp.lua
@@ -67,7 +67,7 @@ cmp.setup {
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+      select = false,
     },
   },
   snippet = {
