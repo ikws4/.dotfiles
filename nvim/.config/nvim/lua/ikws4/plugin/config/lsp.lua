19554f0583519955c198b2dcf304990f89b070f8
ikws4 <zhipingne@gmail.com>
2021-12-03 17:55:05 +0800

    nvim(lsp): disable signs

--- nvim/.config/nvim/lua/ikws4/plugin/config/lsp.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/config/lsp.lua
@@ -53,9 +53,14 @@ lsp_installer.on_server_ready(function(server)
   vim.cmd [[ do User LspAttachBuffers ]]
 end)
 
+vim.diagnostic.config {
+  underline = false,
+  signs = false,
+}
+
 -- replace the default lsp diagnostic symbols
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "LineNr" })
-end
+-- local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+-- for type, icon in pairs(signs) do
+--   local hl = "DiagnosticSign" .. type
+--   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "LineNr" })
+-- end
