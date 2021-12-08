23af758be60f402b4fdd96d59cac3eaf91ed502c
ikws4 <zhipingne@gmail.com>
2021-12-08 17:46:36 +0800

    nvim(lsp): jdtls formatter setting

--- nvim/.config/nvim/lua/ikws4/plugin/config/lsp.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/config/lsp.lua
@@ -40,8 +40,25 @@ lsp_installer.on_server_ready(function(server)
 
   if server.name == "sumneko_lua" then
     opts = require("lua-dev").setup {
-      lspconfig = opts
+      lspconfig = opts,
     }
+  end
+
+  if server.name == "jdtls" then
+    opts = vim.tbl_deep_extend("keep", opts, {
+      settings = {
+        java = {
+          format = {
+            comments = {
+              enabled = false,
+            },
+            settings = {
+              url = "https://gist.githubusercontent.com/ikws4/7880fdcb4e3bf4a38999a628d287b1ab/raw/8e7f86e5ece156345628382cfa5c124322b38b5b/jdtls-formatter-style.xml",
+            },
+          },
+        },
+      },
+    })
   end
 
   server:setup(opts)
