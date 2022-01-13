f74044c918c774ad88e5b9a00631c94e31ee4e1c
ikws4 <zhipingne@gmail.com>
2022-01-14 00:02:52 +0800

    nvim(packer): auto exec PackerCompile

--- nvim/.config/nvim/lua/ikws4/plugin/packer_init.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/packer_init.lua
@@ -50,11 +50,11 @@ packer.init {
   compile_on_sync = true,
 }
 
--- cmd [[
---   augroup PackerCompileAutoGroup
---     autocmd!
---     autocmd BufWritePost */nvim/lua/ikws4/*.lua source <afile> | PackerCompile
---   augroup END
--- ]]
+cmd [[
+  augroup PackerCompileAutoGroup
+    autocmd!
+    autocmd BufWritePost */nvim/lua/ikws4/plugin/init.lua source <afile> | PackerCompile
+  augroup END
+]]
 
 return packer
