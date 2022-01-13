9bfd200bdac22ee68bb663b956702c08ec1a294a
ikws4 <zhipingne@gmail.com>
2022-01-13 12:50:00 +0800

    nvim(harpoon): use <leader>hh to open ui

--- nvim/.config/nvim/lua/ikws4/plugin/init.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/init.lua
@@ -315,9 +315,13 @@ return packer.startup(function()
       local nnoremap = vim.keymap.nnoremap
 
       nnoremap { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>" }
-      nnoremap { "<leader>hw", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>" }
+      nnoremap { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>" }
       nnoremap { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>" }
       nnoremap { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>" }
+      nnoremap { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>" }
+      nnoremap { "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>" }
+      nnoremap { "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>" }
+      nnoremap { "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>" }
     end,
   }
   -- }}}
