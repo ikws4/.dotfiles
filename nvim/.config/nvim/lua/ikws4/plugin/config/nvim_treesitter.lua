7823bbe906fb4770c5252608ed4048df970a5744
ikws4 <zhipingne@gmail.com>
2022-01-22 21:09:34 +0800

    nvim(indent): disable indent feature for java

--- nvim/.config/nvim/lua/ikws4/plugin/config/nvim_treesitter.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/config/nvim_treesitter.lua
@@ -39,7 +39,7 @@ require("nvim-treesitter.configs").setup {
   },
   indent = {
     enable = true,
-    disable = { "python" },
+    disable = { "python", "java" },
   },
   textobjects = {
     select = {
