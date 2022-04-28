10e9cde180e0937e8890e7c0adbbfd5bf098ba56
ikws4 <zhipingne@gmail.com>
2022-04-28 11:07:59 +0800

    nvim(fugitive): hide number line

--- nvim/.config/nvim/after/ftplugin/fugitive.lua
+++ nvim/.config/nvim/after/ftplugin/fugitive.lua
@@ -1,2 +1,7 @@
+local vim = vim
+
 vim.keymap.set("n", "<Esc>", "<Cmd>q<CR>", { buffer = true })
 vim.keymap.set("n", "<Tab>", "=", { buffer = true, remap = true })
+
+vim.wo.number = false
+vim.wo.relativenumber = false
