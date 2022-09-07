cc8cbd3d0dc1d698c0652cd85e601137f3e1e611
ikws4 <zhipingne@gmail.com>
2022-09-07 11:25:22 +0800

    fish: update keybing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -64,4 +64,6 @@ function fish_user_key_bindings
   fish_vi_key_bindings
   bind -M insert -m default jk backward-char force-repaint
   bind -M insert \el accept-autosuggestion
+  bind -M insert \ej down-or-search
+  bind -M insert \ek up-or-search
 end
