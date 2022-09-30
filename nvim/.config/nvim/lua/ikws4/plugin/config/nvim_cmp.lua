79000c0b6d440d56560d8ea526b5657f732b0aab
ikws4 <zhipingne@gmail.com>
2022-09-30 11:23:21 +0800

    nvim(cmp): smooth docs scrolling

--- nvim/.config/nvim/lua/ikws4/plugin/config/nvim_cmp.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/config/nvim_cmp.lua
@@ -99,8 +99,8 @@ cmp.setup {
     }),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(1),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-1),
     ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
