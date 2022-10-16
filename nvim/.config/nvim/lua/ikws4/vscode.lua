b49dd8aa211a6a868f10f1473fe8c704b3027ae8
ikws4 <zhipingne@gmail.com>
2022-10-16 20:30:45 +0800

    nvim: update vscode config

--- nvim/.config/nvim/lua/ikws4/vscode.lua
+++ nvim/.config/nvim/lua/ikws4/vscode.lua
@@ -9,18 +9,32 @@ o.tabstop = 2
 o.shiftwidth = 2
 o.expandtab = true
 
+o.clipboard = "unnamedplus"
+
 -- keymappings
-local function k(a)
+local function k(...)
+  local args = table.pack(...)
   return function()
-    vim.cmd("call " .. "VSCodeNotify('" .. a .. "')")
+    for _, cmd in ipairs(args) do
+      vim.cmd("call " .. "VSCodeNotify('" .. cmd .. "')")
+    end
   end
 end
 
-vim.keymap.set("n", "gD", k "editor.action.goToDeclaration")
-vim.keymap.set("n", "gd", k "editor.action.goToDefinition")
+vim.keymap.set("n", "gD", k "editor.action.goToTypeDeclaration")
+vim.keymap.set("n", "gd", k "editor.action.goToDeclaration")
 vim.keymap.set("n", "gr", k "editor.action.goToReferences")
 vim.keymap.set("n", "gi", k "editor.action.goToImplementation")
 vim.keymap.set("i", "<C-K>", k "editor.action.triggerSuggest")
 vim.keymap.set("n", "<leader>lr", k "editor.action.rename")
 vim.keymap.set("n", "<leader>la", k "editor.action.quickFix")
 vim.keymap.set("n", "<leader>lf", k "editor.action.format")
+
+vim.keymap.set("n", "<leader>m", k "workbench.action.terminal.focus")
+vim.keymap.set(
+  "n",
+  "<leader>n",
+  k("workbench.files.action.focusFilesExplorer", "workbench.files.action.showActiveFileInExplorer")
+)
+
+vim.keymap.set("n", "<leader>ff", k "workbench.action.quickOpen")
