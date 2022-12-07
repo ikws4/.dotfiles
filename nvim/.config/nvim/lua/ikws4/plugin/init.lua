6d97f64ead7fbbb2b8e15519daa7e516374e9501
ikws4 <zhipingne@gmail.com>
2022-12-07 10:29:48 +0800

    feat: add '<' '>' auto pair for rust and java

--- nvim/.config/nvim/lua/ikws4/plugin/init.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/init.lua
@@ -414,9 +414,15 @@ return packer.startup(function()
       local cmp_autopairs = require "nvim-autopairs.completion.cmp"
       local cmp = require "cmp"
       local npairs = require "nvim-autopairs"
+      local Rule = require('nvim-autopairs.rule')
+      local cond = require('nvim-autopairs.conds')
       npairs.setup()
 
       npairs.add_rules(require "nvim-autopairs.rules.endwise-lua")
+      npairs.add_rule(
+        Rule("<", ">", { 'rust', 'java' })
+        :with_pair(cond.not_before_regex(" "))
+      )
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
     end,
   }
