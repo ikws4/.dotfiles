655e750d62a6130d7cf7f40bffbf016eda8c708e
ikws4 <zhipingne@gmail.com>
2022-12-11 16:45:28 +0800

    nvim: fix '<' auto pair

--- nvim/.config/nvim/lua/ikws4/plugin/init.lua
+++ nvim/.config/nvim/lua/ikws4/plugin/init.lua
@@ -433,12 +433,17 @@ return packer.startup(function()
       npairs.setup()
 
       npairs.add_rules(require "nvim-autopairs.rules.endwise-lua")
-      npairs.add_rule(Rule("<", ">", { "rust", "java" }):with_pair(cond.not_before_regex " "):with_move(function(opts)
-        if opts.char == opts.next_char then
-          return
-        end
-        return false
-      end))
+      npairs.add_rule(
+        Rule("<", ">", { "rust", "java" })
+          :with_pair(cond.not_before_regex " ")
+          :with_pair(cond.not_after_regex [=[[%w%%%'%[%"%.%`%$]]=])
+          :with_move(function(opts)
+            if opts.char == opts.next_char then
+              return
+            end
+            return false
+          end)
+      )
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
     end,
   }
